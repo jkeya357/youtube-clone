1ba187779d62f2fb1ef76bc8322263b0137e4e5e
Joshua <jkeya357@gmail.com>
2025-04-07 00:29:47 +0200

    YouTube-Clone1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/HOME.JSX'
+import Video from './Pages/Video/Video'
+import { useState } from 'react'
+
+function App() {
+
+  const [sidebar, setSidebar] = useState(true);
+
+  return (
+    <div>
+      <NavBar setSidebar={setSidebar}/>
+      <Routes>
+        <Route path='/' element={<Home sidebar={sidebar}/>}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App

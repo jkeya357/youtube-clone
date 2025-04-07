f92f4e9c138c8a468cfae8529256d11030b87af5
Joshua <jkeya357@gmail.com>
2025-04-07 20:25:06 +0200

    fixed the import for Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home/HOME.JSX'
+import Home from './Pages/Home/Home'
 import Video from './Pages/Video/Video'
 import { useState } from 'react'
 
